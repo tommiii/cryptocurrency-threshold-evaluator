600269048c62ca93b2f7d8a8ccdb0538b7db8b10
tommiii <36520669+tommiii@users.noreply.github.com>
2019-09-26 21:12:30 +0200

    other dependencis, eslint config, redux structure init

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Hello There! This is my app!
+    </div>
+  );
+}
+
+export default App;
